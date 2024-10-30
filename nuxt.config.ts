516e8e2678799e7869c225fadd203f693df5807d
Zakki Haris <zakki.abdurrahman@talenavi.com>
2024-10-30 11:57:03 +0700

    feat: [ZH] setting up progressive web apps

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -6,8 +6,37 @@ export default defineNuxtConfig({
     '@vite-pwa/nuxt'
   ],
   pwa: {
-    /* PWA options */
+    strategies: 'generateSW',
+    manifest: {
+      name: 'Diamond Clinic',
+      short_name: 'DiamonClinic',
+      description: 'Diamond Beauty & Anti Aging Clinic',
+      theme_color: '#ffffff',
+      background_color: '#ffffff',
+      display: 'standalone',
+      orientation: 'portrait',
+      icons: [
+        {
+          src: 'pwa-192x192.png',
+          sizes: '192x192',
+          type: 'image/png',
+        },
+        {
+          src: 'pwa-512x512.png',
+          sizes: '512x512',
+          type: 'image/png',
+        },
+        {
+          src: 'pwa-512x512.png',
+          sizes: '512x512',
+          type: 'image/png',
+          purpose: 'any maskable',
+        },
+      ],
+    },
+    workbox: {
+      cleanupOutdatedCaches: true,
+      navigateFallback: '/',
+    }
   },
-
-  compatibilityDate: '2024-10-24'
 })
