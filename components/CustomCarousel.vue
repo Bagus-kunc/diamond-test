782b3eb07e8e6f201f1b6141b15e20086bd00af4
Zakki Haris <zakki.abdurrahman@talenavi.com>
2024-11-06 09:43:38 +0700

    feat: [ZH] auto play video iframe youtube

--- components/CustomCarousel.vue
+++ components/CustomCarousel.vue
@@ -7,6 +7,7 @@
       :showIndicators="false"
       class="h-full"
       contentClass="max-h-[110vh] max-w-[110vh]"
+      @update:page="controlVideo"
     >
       <template #item="{ data }">
         <div
@@ -32,7 +33,7 @@
             >
               <iframe
                 class="top-0 left-0 w-full h-full"
-                :src="data.url"
+                :src="`${data.url}?rel=0&enablejsapi=1`"
                 title="YouTube video player"
                 allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
                 referrerpolicy="strict-origin-when-cross-origin"
@@ -47,7 +48,7 @@
 </template>
 
 <script setup>
-import { ref } from "vue";
+import { ref, nextTick } from "vue";
 
 const products = ref([
   {
@@ -68,7 +69,25 @@ const products = ref([
   {
     id: "4",
     type: "iframe",
-    url: "https://www.youtube.com/embed/D7Q4r3r_pXw?si=5fZPfxxC2vXPr6XG&controls=0&modestbranding=1&showinfo=0",
+    url: "https://www.youtube.com/embed/D7Q4r3r_pXw",
   },
 ]);
+
+const controlVideo = async (index) => {
+  await nextTick();
+
+  let vidFunc =
+    products.value[index]?.type === "iframe" ? "playVideo" : "stopVideo";
+
+  const iframe = document.getElementsByTagName("iframe")[0];
+
+  if (iframe && iframe.contentWindow) {
+    iframe.contentWindow.postMessage(
+      '{"event":"command","func":"' + vidFunc + '","args":""}',
+      "*"
+    );
+  }
+
+  console.info("vidFunc", vidFunc);
+};
 </script>
