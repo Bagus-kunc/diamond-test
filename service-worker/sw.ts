9401b12343041ac536f84f5bff56a51f62a0e58a
bayuaji676 <bayuaji676@gmail.com>
2024-11-08 17:35:00 +0700

    fix: [BA] offline mode

--- service-worker/sw.ts
+++ service-worker/sw.ts
@@ -1,17 +1,18 @@
 /// <reference lib="WebWorker" />
 /// <reference types="vite/client" />
-import { cleanupOutdatedCaches, createHandlerBoundToURL, precacheAndRoute } from 'workbox-precaching'
-import { clientsClaim } from 'workbox-core'
-import { NavigationRoute, registerRoute } from 'workbox-routing'
+import { cleanupOutdatedCaches, createHandlerBoundToURL, precacheAndRoute } from 'workbox-precaching';
+import { clientsClaim, cacheNames } from 'workbox-core';
+import { NavigationRoute, registerRoute } from 'workbox-routing';
 
 // Declare self for TypeScript
-declare let self: ServiceWorkerGlobalScope
+declare let self: ServiceWorkerGlobalScope;
 
 // Define custom cache name and assets
-const CACHE_NAME = 'diamond-clinic-cache-v2';
+const CACHE_NAME = cacheNames.precache;
 const ASSETS_TO_CACHE = [
   '/',
   'https://fonts.googleapis.com/css2?family=Montserrat:ital,wght@0,100..900;1,100..900&display=swap',
+  'https://fonts.googleapis.com/css2?family=Lato:wght@300;400;700;900&display=swap',
 ];
 
 // Precache assets defined in __WB_MANIFEST
@@ -20,12 +21,13 @@ precacheAndRoute(self.__WB_MANIFEST);
 // Cache additional static assets during installation
 self.addEventListener('install', (event) => {
   event.waitUntil(
-    caches.open(CACHE_NAME)
+    caches
+      .open(CACHE_NAME)
       .then((cache) => {
         console.log('Opened cache and caching additional assets');
         return cache.addAll(ASSETS_TO_CACHE);
       })
-      .catch((error) => console.error('Caching failed', error))
+      .catch((error) => console.error('Caching failed', error)),
   );
 });
 
@@ -41,9 +43,9 @@ self.addEventListener('activate', (event) => {
             console.log('Deleting old cache:', cacheName);
             return caches.delete(cacheName);
           }
-        })
+        }),
       );
-    })
+    }),
   );
 });
 
@@ -52,7 +54,7 @@ self.addEventListener('fetch', (event) => {
   event.respondWith(
     caches.match(event.request).then((response) => {
       return response || fetch(event.request);
-    })
+    }),
   );
 });
 
@@ -60,12 +62,7 @@ self.addEventListener('fetch', (event) => {
 let allowlist: undefined | RegExp[];
 if (import.meta.env.DEV) allowlist = [/^\/$/];
 
-registerRoute(
-  new NavigationRoute(
-    createHandlerBoundToURL('/'),
-    { allowlist }
-  )
-);
+registerRoute(new NavigationRoute(createHandlerBoundToURL('/'), { allowlist }));
 
 self.skipWaiting();
 clientsClaim();
