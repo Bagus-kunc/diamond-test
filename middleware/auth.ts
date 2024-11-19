d8ec1ecdabaa6c0e879994750cc152e47a57ac58
bayuajitalenavi <bayu.aji@talenavi.com>
2024-11-19 17:21:59 +0700

    fix: [BA] offline mode

--- middleware/auth.ts
+++ middleware/auth.ts
@@ -1,6 +1,17 @@
 export default defineNuxtRouteMiddleware(async () => {
   const TOKEN = useCookie('TOKEN');
 
+  if (TOKEN.value) {
+    const isValid = verifyToken(TOKEN.value);
+
+    if (!isValid) {
+      setTimeout(() => {
+        TOKEN.value = null;
+        return navigateTo('/');
+      }, 500);
+    }
+  }
+
   if (!TOKEN.value) {
     return navigateTo('/');
   }
